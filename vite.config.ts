ce05b185425de84b1317ba106e0aa25b53d34af5
itsamejms <james.twose@alliander.com>
2025-11-29 11:05:03 +0000

    updating to use the gh pages base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/seamless-website/" : "/",
   server: {
     host: "::",
     port: 5173,
